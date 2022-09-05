a7835b0a8083c0e39c4005f8ae7a2ef3f8a50561
LAPTOP-RL15OQN5\krist <wi21b037@technikum-wien.at>
2022-09-05 17:59:20 +0200

    Initial commit

---
+++ src/main/java/com/example/FlaechenberechnungsAPI/FlaechenberechnungsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlaechenberechnungsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaechenberechnungsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlaechenberechnungsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FlaechenberechnungsAPI/FlaechenberechnungsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlaechenberechnungsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlaechenberechnungsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
